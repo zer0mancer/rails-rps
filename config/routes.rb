c38a08e837fb219fff452e1d63b88506dd6a6778
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2024-11-26 02:46:55 +0000

    fixed links to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get("/", {:controller=> "game", :action=>"homepage"})
-  get("/rocks", {:controller=> "game", :action=>"rocks"})
+  get("/rock", {:controller=> "game", :action=>"rocks"})
   get("/scissors", {:controller=> "game", :action=>"scissors"})
-  get("/papers", {:controller=> "game", :action=>"papers"})
+  get("/paper", {:controller=> "game", :action=>"papers"})
 
 
 
